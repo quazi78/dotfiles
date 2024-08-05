81437504456e309257a8c2fa6a63ec15bf00551d
Blake Cheatham <blake.lag@gmail.com>
2024-08-05 14:26:54 -0400

    use stow and nvim

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,23 @@
+vim.cmd("set number")
+vim.cmd("set relativenumber")
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+-- vim.cmd("set showtabline=2")
+vim.cmd("set laststatus=2")
+vim.cmd("set hidden")
+vim.cmd("set noshowmode")
+vim.cmd("set ic")
+vim.cmd("set hlsearch")
+vim.cmd("nnoremap ; :")
+vim.g.mapleader = " "
+
+require("config.lazy")
+
+local builtin = require("telescope.builtin")
+vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<Tab>', ':bnext<CR>', {})
+
+vim.cmd.colorscheme "catppuccin"
---
+++ nvim/.config/nvim/lua/config/treesitter.lua
@@ -0,0 +1,14 @@
+require("lazy").setup({{
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function () 
+      local configs = require("nvim-treesitter.configs")
+
+      configs.setup({
+          ensure_installed = { "lua", "vim", "vimdoc", "html", "python", "terraform", "tmux" },
+          sync_install = false,
+          highlight = { enable = true },
+          indent = { enable = true },  
+        })
+    end
+ }})
