51d32c56c2224b0c99a63d7e34b8806b54034e39
Blake C <blake.lag@gmail.com>
2025-09-02 12:00:40 -0400

    update vim plugins and kitty colors

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -20,4 +20,4 @@ vim.keymap.set('n', '<C-p>', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<Tab>', ':bnext<CR>', {})
 
-vim.cmd.colorscheme "catppuccin"
+vim.cmd.colorscheme "carbonfox"
